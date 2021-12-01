2ee9ab51f78519ef00f2d2cef8eb56aee2a350ae
cheunyinz <83555076+cheunyinz@users.noreply.github.com>
2021-12-01 18:27:39 +0100

    nav bar 95% af en main toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
 // JavaScript Document
+
+const openMenuBtn = document.querySelector("header nav>button")
+const closeMenuBtn= document.querySelector ("header>nav>section:last-of-type>button")
+const navMenu = document.querySelector('header nav>section:last-of-type')
+
+function toggleMenu() {
+    navMenu.classList.toggle('openMenu')
+    console.log ("test")
+}
+
+openMenuBtn.addEventListener ('click', toggleMenu)
+closeMenuBtn.addEventListener ('click', toggleMenu)
